5c1833845e4c68f454b693f217192bd691d64d47
illuminateor <o.mudrak93@gmail.com>
2024-04-07 01:17:56 +0200

    Add create note + index notes

---
+++ resources/views/livewire/notes/edit-note.blade.php
@@ -0,0 +1,12 @@
+<?php
+
+use Livewire\Volt\Component;
+use Livewire\Attributes\Layout;
+
+new #[layout('layouts.app')] class extends Component {
+    //
+}; ?>
+
+<div>
+    <p>Hi There</p>
+</div>
